26fbb32cce247f2d946f001b68af66958a1340eb
AronZeng <aron.1998.zeng@gmail.com>
2020-07-13 00:15:14 -0400

    Both APIs functional, still need to cleanup

--- controller/weather.js
+++ controller/weather.js
@@ -2,6 +2,7 @@ const express = require("express");
 const req = require("request");
 const moment = require("moment");
 const constants = require("./helpers/constants");
+const cityList = require("../city.list");
 require("dotenv").config();
 var {
   preconditionErrorCheck,
@@ -47,21 +48,44 @@ exports.currentAverage = async function (request, response) {
   try {
     let cities = JSON.parse(request.params.cities);
     console.log(cities);
-    let citiesString = cities[0];
-    for (var i = 1; i < cities.length; i++) {
-      citiesString = citiesString + "," + cities[i];
+    let cityIds = "";
+    for (var i = 0; i < cities.length; i++) {
+      let city = cityList.find((city) => {
+        return city.name == cities[i];
+      });
+      if (city && city.id) {
+        if (cityIds == "") {
+          cityIds = cityIds + city.id;
+        } else {
+          cityIds = cityIds + "," + city.id;
+        }
+      }
     }
+    console.log(cityIds);
     req(
       process.env.OPEN_WEATHER_URL +
-        `weather?q=${citiesString}&appid=${
-          process.env.OPEN_WEATHER_API_KEY
-        }&units=${request.query.units || "metric"}`,
+        `group?id=${cityIds}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=${
+          request.query.units || "metric"
+        }`,
       (error, res, body) => {
         if (error) {
           return returnResponse(response, 500, {}, error.message);
         }
         var obj = JSON.parse(body);
-        console.log(obj);
+        let sumHighs = 0;
+        let sumLows = 0;
+        for (var i = 0; i < obj.cnt; i++) {
+          sumHighs = sumHighs + obj.list[i].main.temp_max;
+          sumLows = sumLows + obj.list[i].main.temp_min;
+        }
+        let data = {
+          cities: cities,
+          unit: request.query.units == "imperial" ? "Ferinheight" : "Celcius",
+          date: moment().format("MMM Do YYYY"),
+          average_high: sumHighs / obj.cnt,
+          average_low: sumLows / obj.cnt,
+        };
+        return returnResponse(response, 200, data, undefined);
       }
     );
   } catch (error) {
